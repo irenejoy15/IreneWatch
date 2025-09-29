591f2043aa1c0c148f3926e92958e593411e7c86
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-09-29 16:36:49 +0800

    CREATING JSON RESPONSE

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    movie_data = {'movies': list(movies.values())}
+    return JsonResponse(movie_data, safe=False)
