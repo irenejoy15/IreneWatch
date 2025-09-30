c5db121e06e155b7e58f013cdb8b705624f7bba8
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-09-30 14:07:10 +0800

    STATUS CODES

--- IreneWatch/api/view.py
+++ IreneWatch/api/view.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from watchlist_app.models import Movie
 from IreneWatch.api.serializers import MovieSerializer
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 @api_view(['GET','POST'])
 def movie_list(request):
@@ -16,15 +17,18 @@ def movie_list(request):
             serialized_data.save()
             return Response(serialized_data.data)
         else:
-            return Response(serialized_data.errors)
+            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
 def movie_detail(request, pk):
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
-        serialized_data = MovieSerializer(movie)
-        return Response(serialized_data.data)
-    
+        try:
+            movie = Movie.objects.get(pk=pk)
+            serialized_data = MovieSerializer(movie)
+            return Response(serialized_data.data)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'PUT':
         movie = Movie.objects.get(pk=pk)
         serialized_data = MovieSerializer(movie, data=request.data)
@@ -32,9 +36,9 @@ def movie_detail(request, pk):
             serialized_data.save()
             return Response(serialized_data.data)
         else:
-            return Response(serialized_data.errors)
+            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
         
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response({'message': 'Movie deleted successfully!'})
+        return Response(status=status.HTTP_204_NO_CONTENT, data= {'message': 'Movie deleted successfully!'})
