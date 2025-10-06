ab4fbad27391b790026bb1bc3d5ef3b2b27c340a
Cefrian Yves Trinchera <cefrian.trinchera@SANPABLO.LOCAL>
2025-10-07 07:41:17 +0800

    Custom Serializers

--- IreneWatch/api/serializers.py
+++ IreneWatch/api/serializers.py
@@ -2,11 +2,16 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
         fields = '__all__'
         # fields = ['id', 'name', 'description', 'active']
         # exclude = ['active']
+
+    def get_len_name(self, object):
+        return len(object.name)
 
     def validate(self, data):
         if data['name'] == data['description']:
